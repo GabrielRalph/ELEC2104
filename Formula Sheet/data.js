aac6d23d55635684fa5ec66703dc6e9995de761a
GabrielRalph <gltralph@gmail.com>
2022-09-13 21:23:31 +1000

    fsheet

---
+++ Formula Sheet/data.js
@@ -0,0 +1,15 @@
+let Vars = {
+  "I_s": {
+    name: "Saturation current",
+
+  },
+  "A": {
+    name: "Area"
+  }
+
+}
+
+let Eqs = {
+  "I_s = \frac{A q D_n n_i^2} {N_A N_D}",
+  "I_D = I_S[e^\frac{V_DE}{V_T} - 1]"
+}
