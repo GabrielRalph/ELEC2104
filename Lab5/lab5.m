b49244c8d1ced3d38a85efc0604b768e4f5fcbb4
GabrielRalph <gltralph@gmail.com>
2022-10-12 16:48:22 +1100

    Lab 5

--- Lab5/lab5.m
+++ Lab5/lab5.m
@@ -5,9 +5,9 @@ w2.plot()
 w2.save("svg");
 %% Question 2
 va = {
-    ELVISLogs("Q2/Va = 4V.txt");
-    ELVISLogs("Q2/Va = 5V.txt");
-    ELVISLogs("Q2/Va = 8V.txt");
+    ELVISLogs("Lab5/Q2/Va = 4V.txt");
+    ELVISLogs("Lab5/Q2/Va = 5V.txt");
+    ELVISLogs("Lab5/Q2/Va = 8V.txt");
 };
 va0 = va{1};
 for i = 2:3
@@ -15,9 +15,9 @@ for i = 2:3
 end
 va0.plot()
 legend(["V_A = 4V", "V_A = 5V", "V_A = 8V"]);
-
+title("MOSFET 2 Wire Analysis");
 %% Question 3
-names = ["Q3/LED test 5Hz.txt"; "Q3/LED test 10Hz.txt"; "Q3/LED test 1Hz.txt"];
+names = ["Lab5/Q3/LED test 5Hz.txt"; "Lab5/Q3/LED test 10Hz.txt"; "Lab5/Q3/LED test 1Hz.txt"];
 n = 3;
 for i = 1:3
     subplot(3, 1, i);
@@ -39,3 +39,15 @@ end
  V_GS = sqrt(i_ref/beta) + V_TO;
  R_ref = (Vdd - V_GS) / i_ref;
  fprintf("R_ref = %.2fkR\n", R_ref/1000);
+ %% Question 5
+% names = ["Lab5/Q3/LED test 5Hz.txt"; "Lab5/Q3/LED test 10Hz.txt"; "Lab5/Q3/LED test 1Hz.txt"];
+% n = length(names);
+logs = ELVISLogs.getAll("Lab5/Q5");
+%%
+n = length(logs);
+for i = 1:n
+    subplot(n, 1, i);
+    logs(i).plot();
+    logs(i).signals = abs(logs(i).signals);
+    legend(["fgen", "V_{out}"]);
+end
